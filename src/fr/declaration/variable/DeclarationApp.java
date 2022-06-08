7ca11b6052034c4cb06e4dce082d0b44fcf80414
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-06-08 14:54:07 +0200

    Declaration.

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp
+{
+	public static void main(String[] args)
+	{
+		byte a = 1;
+		short b = 2;
+		int c = 3;
+		long d = 4;
+		float e = 5.0f;
+		double f = 6.0;
+		char g = '7';
+		boolean h = true;
+		String i = "i",
+				randomString = "Résultat:\n 1 + 5 = " + (1 + 5);
+		
+		System.out.println(randomString);
+	}
+}
